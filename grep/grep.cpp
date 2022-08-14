c557510d284f5032802ca2fadbe65b61c474c4e5
Culex <culex.jj@gmail.com>
2022-08-14 12:37:07 +0200

    V: 0.1 - 14/08/2022

--- grep/grep.cpp
+++ grep/grep.cpp
@@ -1,12 +1,68 @@
 // grep.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
 
-int main()
+/********** Search char string on file/s **********/
+/* grep.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int BuscarCadena(char* cadena1, char* cadena2)
 {
-    std::cout << "Hello World!\n";
+    // ¿cadena2 está contenida en cadena1?
+    if (strstr(cadena1, cadena2))
+        return 1; // se encontró
+    else
+        return 0; // no se encontró
 }
+
+void BuscarEnFich(char* nombrefich, char* cadena)
+{
+    FILE* pf;
+    char linea[256];
+    int nrolinea = 0;
+    int control = 1; //asumimos error en la apertura
+
+    // Abrir el fichero nombrefich
+
+    control = fopen_s(&pf, nombrefich, "r"); // si ok control = 0
+
+
+    if ( control != 0)
+    {
+        perror(nombrefich);
+        return;
+    }
+
+    // Buscar cadena en el fichero asociado con pf
+    while (fgets(linea, sizeof(linea), pf) != NULL)
+    {
+        nrolinea++;
+        if (BuscarCadena(linea, cadena))
+            printf("%s[%d]: %s", nombrefich, nrolinea, linea);
+    }
+    fclose(pf);
+}
+
+int main(int argc, char* argv[])
+{
+    int i;
+
+    // Verificar el número de argumentos
+    if (argc < 3)
+    {
+        printf("Sintaxis: %s palabra f1 f2 ... fn\n", argv[0]);
+        exit(-1);
+    }
+
+    // Llamar a la función BuscarEnFich por cada fichero
+    for (i = 2; i < argc; i++)
+        BuscarEnFich(argv[i], argv[1]);
+}
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
